cbbab9759b33c7460a7eac160460fa9e1733cbf3
Phạm Văn Nguyên <npv2k1@gmail.com>
2023-08-04 23:48:50 +0700

    Change scence function

--- src/main/java/com/example/kindergartenmanager/dao/DBUtils.java
+++ src/main/java/com/example/kindergartenmanager/dao/DBUtils.java
@@ -17,34 +17,9 @@ public class DBUtils {
             String url = "jdbc:sqlserver://DESKTOP-9J07U8P\\SQLEXPRESS:1433;"+"user=sa;password=123;databaseName=Kindergarten;encrypt=true;trustServerCertificate=true";
             Connection connection = DriverManager.getConnection(url);
             return connection;
-
-//            return connection;
         } catch (Exception e) {
             e.printStackTrace();
         }
         return null;
     }
-
-    public static void changeScence(ActionEvent event, String fxmlFile, String title, String username, String password) {
-        Parent root = null;
-        if(username != null && password != null) {
-            try {
-                FXMLLoader loader = new FXMLLoader(DBUtils.class.getResource(fxmlFile));
-                root  = loader.load();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        } else {
-            try {
-                root = FXMLLoader.load(DBUtils.class.getResource(fxmlFile));
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        stage.setTitle(title);
-        stage.setScene(new Scene(root, 700, 500));
-        stage.show();
-    }
-
 }
