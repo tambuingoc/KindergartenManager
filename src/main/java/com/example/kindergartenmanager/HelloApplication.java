eba7958b0cf0a587e94e0268b38dc248df5aa5a8
Phạm Văn Nguyên <npv2k1@gmail.com>
2023-08-05 12:18:04 +0700

    Fix init scence

--- src/main/java/com/example/kindergartenmanager/HelloApplication.java
+++ src/main/java/com/example/kindergartenmanager/HelloApplication.java
@@ -1,5 +1,6 @@
 package com.example.kindergartenmanager;
 
+import com.example.kindergartenmanager.helper.Helper;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -10,7 +11,9 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login.fxml"));
+        String packageName= "/com/example/kindergartenmanager/";
+        String initScence = "login.fxml";
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource(packageName+initScence));
         Scene scene = new Scene(fxmlLoader.load(), 1100, 600);
         stage.setTitle("Hello!");
         stage.setScene(scene);
